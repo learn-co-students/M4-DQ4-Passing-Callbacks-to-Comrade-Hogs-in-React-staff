6c2a1baa82e2859cc47fee2abd0975aac5df5908
vibraniumforge <gregm9999@gmail.com>
2020-02-08 16:07:14 -0800

    Done.

--- src/components/GalaxySNote7.js
+++ src/components/GalaxySNote7.js
@@ -1,42 +1,52 @@
-import React from "react"
-import wreee from '../assets/wreee.mp3';
-import exclaim from '../assets/exclaim.mp3';
-import exclamation from "../assets/exclamation.png"
-
+import React from "react";
+import wreee from "../assets/wreee.mp3";
+import exclaimShort from "../assets/exclaim.mp3";
+import exclamation from "../assets/exclamation.png";
 
 export default class GalaxySNote7 extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      panicked: false,
-    }
+      panicked: this.props.environment === "inhospitable"
+    };
 
     this.squeelAudio = new Audio(wreee);
-    this.exclaimAudio = new Audio(exclaim);
-    this.exclaimAudio.addEventListener("ended", () => {
-      this.throwAFit()
-    }, false)
-  }
-
-  throwAFit = () => {
+    this.exclaimAudio = new Audio(exclaimShort);
+    this.exclaimAudio.addEventListener(
+      "ended",
+      () => {
+        this.props.alterEnvironment("inhospitable");
+      },
+      false
+    );
   }
 
   relax = () => {
-  }
+    this.setState({ panicked: false });
+  };
 
-  exclaim = () => {
-    if (this.state.panicked) return
-    this.exclaimAudio.play()
-    this.squeelAudio.play()
-  }
+  panic = () => {
+    if (this.state.panicked) return;
+    this.setState({ panicked: true });
+    setTimeout(this.relax, 3500);
+    this.exclaimAudio.play();
+    this.squeelAudio.play();
+  };
 
-  panic = () => (<img id="galaxy-exclamation" className="exclamation" src={exclamation} alt="" />)
+  exclaim = () => (
+    <img
+      id="galaxy-exclamation"
+      className="exclamation"
+      src={exclamation}
+      alt=""
+    />
+  );
 
   render() {
-    return(
-      <div id="galaxy-s-note" onClick={this.exclaim}>
-        {(this.state.panicked) ? this.panic() : null}
+    return (
+      <div id="galaxy-s-note" onClick={this.panic}>
+        {this.state.panicked ? this.exclaim() : null}
       </div>
-    )
+    );
   }
 }
